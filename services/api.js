072b3bab0636130d62245244aaf24172ae78cc86
Austine Amah <austine.amah@gmail.com>
2022-01-24 21:18:50 +0100

    Setup  production baseURL

--- services/api.js
+++ services/api.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://tutorialized.amaniart.net",
 });
 
 export const injectScript = (src) => {
